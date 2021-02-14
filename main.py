9b751ebc2184f22ed89de74394f6d5556e0ebf31
whatever125 <kolmakov125@gmail.com>
2021-02-14 16:52:00 +0700

    Часть 11

--- main.py
+++ main.py
@@ -31,11 +31,15 @@ class MainWindow(QWidget):
         self.map_type = 'map'
         self.index = False
         self.found_index = False
+        self.clicked = False
         self.points = []
         self.update_image()
 
     def find_toponym(self):
         text = self.lineEdit.text()
+        if not text:
+            self.label_2.setText('Пустая строка')
+            return
         search_params = {
             "apikey": search_api_key,
             "text": text,
@@ -52,29 +56,10 @@ class MainWindow(QWidget):
             coordinates = obj['geometry']['coordinates']
             address = obj['properties']['description']
             self.lon, self.lat = coordinates
-            self.points = [*coordinates, 'round']
+            self.points = [*coordinates, 'comma']
             index = ''
             if self.index:
-                index_search_params = {
-                    "apikey": geocode_api_key,
-                    "geocode": ','.join(list(map(lambda x: str(x), coordinates))),
-                    "format": "json"
-                }
-                index_response = requests.get(geocode_api_server, params=index_search_params)
-
-                if not index_response:
-                    self.label_2.setText(index_response.status_code, ' ', index_response.reason)
-                    return
-                try:
-                    index_response = index_response.json()
-                    toponym = index_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                    index = toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
-                except Exception:
-                    self.label_2.setText('Почтовый индекс не найден')
-                    self.found_index = False
-                else:
-                    self.label_2.setText('')
-                    self.found_index = True
+                index = self.find_index(list(map(lambda x: str(x), coordinates)))
             if index:
                 self.lineEdit_2.setText(address + ', ' + index)
             else:
@@ -83,10 +68,40 @@ class MainWindow(QWidget):
         except Exception:
             self.label_2.setText('Ничего не нашлось')
 
+    def find_toponym_on_click(self, lon, lat):
+        search_params = {
+            "apikey": search_api_key,
+            "text": ','.join([lat, lon]),
+            "lang": "ru_RU"
+        }
+        response = requests.get(search_api_server, params=search_params)
+
+        if not response:
+            self.label_2.setText(response.status_code, ' ', response.reason)
+            return
+        try:
+            response = response.json()
+            print(response)
+            obj = response['features'][0]
+            description = obj['properties']['GeocoderMetaData']['text']
+            self.points = [lon, lat, 'comma']
+            index = ''
+            if self.index:
+                index = self.find_index(list(map(lambda x: str(x), [lon, lat])))
+            if index:
+                self.lineEdit_2.setText(description + ', ' + index)
+            else:
+                self.lineEdit_2.setText(description)
+            self.update_image()
+        except Exception:
+            self.label_2.setText('Ничего не нашлось')
+
     def delete_toponym(self):
         self.points = []
+        self.lineEdit.setText('')
         self.lineEdit_2.setText('')
         self.update_image()
+        self.clicked = False
 
     def change_type(self):
         self.map_type = MAP_TYPES[self.comboBox.currentText()]
@@ -95,33 +110,42 @@ class MainWindow(QWidget):
     def show_index(self):
         self.index = bool(1 - self.index)
         if self.index:
-            index_search_params = {
-                "apikey": geocode_api_key,
-                "geocode": ','.join([str(self.lon), str(self.lat)]),
-                "format": "json"
-            }
-            index_response = requests.get(geocode_api_server, params=index_search_params)
-
-            if not index_response:
-                self.label_2.setText(index_response.status_code, ' ', index_response.reason)
-                return
-            try:
-                index_response = index_response.json()
-                toponym = index_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                index = toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
-                self.lineEdit_2.setText(self.lineEdit_2.text() + ', ' + index)
-            except Exception:
-                self.label_2.setText('Почтовый индекс не найден')
-                self.found_index = False
+            if self.clicked:
+                index = self.find_index(self.points[:-1])
             else:
-                self.label_2.setText('')
-                self.found_index = True
+                index = self.find_index([str(self.lon), str(self.lat)])
+            if index:
+                self.lineEdit_2.setText(self.lineEdit_2.text() + ', ' + index)
         else:
             self.label_2.setText('')
             if self.found_index:
                 self.lineEdit_2.setText(','.join(self.lineEdit_2.text().split(',')[:-1]))
             else:
                 pass
+
+    def find_index(self, coordinates):
+        index_search_params = {
+            "apikey": geocode_api_key,
+            "geocode": ','.join(coordinates),
+            "format": "json"
+        }
+        index_response = requests.get(geocode_api_server, params=index_search_params)
+
+        if not index_response:
+            self.label_2.setText(index_response.status_code, ' ', index_response.reason)
+            return
+        try:
+            index_response = index_response.json()
+            toponym = index_response["response"]["GeoObjectCollection"]["featureMember"][0][
+                "GeoObject"]
+            index = toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+            self.label_2.setText('')
+            self.found_index = True
+            return index
+        except Exception:
+            self.label_2.setText('Почтовый индекс не найден')
+            self.found_index = False
+            return ''
 
     def update_image(self):
         self.get_image()
@@ -167,24 +191,35 @@ class MainWindow(QWidget):
                 self.zoom = 0
         if event.key() in [Qt.Key_Up, Qt.Key_Down]:
             if event.key() == Qt.Key_Up:
-                self.lat += 175 / 2 ** self.zoom
+                self.lat += 178.25792 / (2 ** (self.zoom - 1))
             elif event.key() == Qt.Key_Down:
-                self.lat -= 175 / 2 ** self.zoom
+                self.lat -= 178.25792 / (2 ** (self.zoom - 1))
             if self.lat > 85:
                 self.lat = 85
             elif self.lat < -85:
                 self.lat = -85
         if event.key() in [Qt.Key_Left, Qt.Key_Right]:
             if event.key() == Qt.Key_Left:
-                self.lon -= 300 / 2 ** self.zoom
+                self.lon -= ((422.4 / (2 ** (self.zoom - 1))) / 600) * 450
             elif event.key() == Qt.Key_Right:
-                self.lon += 300 / 2 ** self.zoom
+                self.lon += ((422.4 / (2 ** (self.zoom - 1))) / 600) * 450
             if self.lon > 180:
                 self.lon = 180
             elif self.lon < -180:
                 self.lon = -180
         self.update_image()
         event.accept()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            x, y = event.x() - 200, event.y()
+            if 0 <= x <= 450 and 0 <= y <= 450:
+                x_k = (422.4 / (2 ** (self.zoom - 1))) / 600
+                y_k = (178.25792 / (2 ** (self.zoom - 1))) / 450
+                x, y = x - 225, 225 - y
+                lon, lat = round(self.lon + x * x_k, 6), round(self.lat + y * y_k, 6)
+                self.clicked = True
+                self.find_toponym_on_click(str(lon), str(lat))
 
     def closeEvent(self, event):
         pass
